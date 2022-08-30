a14ccb02a430202a198fe18100ec0e5fdcde02de
imadevopstrainer <iamdevopstrainer@gmail.com>
2022-08-30 23:33:28 +0100

    adding figures

--- app.py
+++ app.py
@@ -33,12 +33,12 @@ with file_path.open("rb") as file:
     hashed_passwords=pickle.load(file)
 authenticator = stauth.Authenticate(names,usernames,hashed_passwords,"Indicator_dashboard" ,"abcdef",cookie_expiry_days=30)
 name, authentication_status , username = authenticator.login("login",'main')
+    #------------In case of error
 if authentication_status == False:
     st.error("Username/passwor is incorrect")
 if authentication_status == None:
     st.warning("please enter your username and passwords")
 if authentication_status :
-        ### --- LOAD DATAFRAME
         col1, col2 = st.columns(2)
 
         with col1:
@@ -47,11 +47,12 @@ if authentication_status :
         with col2 :
              authenticator.logout("logout")
 
+    #------------Load data
         def get_data() -> pd.DataFrame:
             uploaded_file = st.file_uploader("Choose a file")
             if uploaded_file is not None:
                 df= pd.read_excel(uploaded_file,'Results')
-                dg= pd.read_excel(uploaded_file,'Raw_DATA')
+                dg= pd.read_excel(uploaded_file,'Raw_DATA',usecols='A:k')
             return df, dg
 
 
@@ -70,8 +71,8 @@ if authentication_status :
             df=df.drop(index=df.index[0],
                 axis=0)
             return df
-        #transform the second dataframe
 
+        #transform the second dataframe
         def transform_second_dataframe(dg)->pd.DataFrame:
             dt= pd.DataFrame()
             dt = dg
@@ -83,26 +84,30 @@ if authentication_status :
             dt["Month"] = dt["Month"].ffill()
             dt["Week"] = dt["Week"].ffill()
             dt["Date"] = dt["Date"].ffill()
+            dt.drop(['PROJ. NAME'], axis=1,inplace=True)
             df2=dt.fillna(value=0)
             df2['Week']=df2['Week'].astype('int')
             df2['Activity']=df2['Activity'].astype(str)
+            df2['MM/WF CODE']=df2['MM/WF CODE'].astype(str)
             df2=df2.sort_values(by=['Activity'])
             activities=df2['Activity'].unique().tolist()
             df2['Duration']=df2['Duration'].astype(str)
             df2['Duration']=df2['Duration'].str.replace('00', '')
             df2['Duration']=df2['Duration'].str.replace(':', '')
             df2['Duration']=df2['Duration'].astype('int')
-            df2.groupby(['Year','Week','Activity'])['Duration'].sum()
+            df2.groupby(['Year','Week','Activity','MM/WF CODE'])['Duration'].sum()
             return df2
 
+        #------------create_second_data
         def create_second_data(df2)-> pd.DataFrame:
 
-            weeks_sum=df2.groupby(['Year','Week','Activity'])[['Duration']].sum().reset_index()
+            weeks_sum=df2.groupby(['Year','Week','Activity','MM/WF CODE'])[['Duration']].sum().reset_index()
             weeks_sum=weeks_sum[weeks_sum['Activity']!='0']
             non_technical=df2[df2['Activity']=='0']
             non_technical=non_technical.groupby(['Year','Week','Task']).agg({'Duration':'sum'}).reset_index()
             return weeks_sum, non_technical
 
+        #------------create_second_data_figures_of_non_technical_data
         def create_figure_of_non_technical_data(non_technical):
 
             w=non_technical['Duration'].to_list()
@@ -134,7 +139,7 @@ if authentication_status :
 
             fig = go.Figure([go.Scatter(x=non_technical['Year&Week'], y=non_technical['Duration'],line_color='#ffe476')])
             fig.update_layout(
-                
+
                 yaxis_title="Hour",
                 xaxis_title="Date",
                 width=1000, height=800,
@@ -171,14 +176,13 @@ if authentication_status :
 
             st.write(fig)
 
+        #------------create_second_data_figures_of_activities
         def create_figure_of_activities(weeks_sum):
-
+        #------------create_activities_dictionary
             w=weeks_sum['Duration'].to_list()
-
             k=[]
             for i in range (0, len(w)) :
                k.append(w[i]/60)
-
             weeks_sum['Duration']=k
             weeks_sum['Year']=weeks_sum['Year'].astype(str)
             weeks_sum['Week']=weeks_sum['Week'].astype(str)
@@ -186,11 +190,12 @@ if authentication_status :
             w=weeks_sum['Activity'].unique().tolist()
             dct={item: weeks_sum[weeks_sum['Activity']==item] for key,item in enumerate(w) }
 
-            #dct.get('MFG').insert(loc=len(dct.get('MFG').columns), column='Average', value=10)
+        #------------Insert_average_column
             dq={'CTO':5,'ETO':10,'MFG':6, 'PLCM':10, 'STD':8}
             for key,val in dq.items() :
                 dct.get(key).insert(loc=len(dct.get(key).columns), column='Average', value=val)
 
+        #------------create_figures_of_activities
 
             for i in w :
                 new_title = f'<p style="font-family:sans-serif; color:black; font-size: 30px;"> {i} figure :</p>'
@@ -243,6 +248,38 @@ if authentication_status :
 
 
                 st.write(fig)
+        #------------create_table_of_activities
+            for i in w:
+                fig = go.Figure(data=[go.Table(header=dict(values=['year','Acitivity','MM/WF CODE','Duration ','Average']),
+                                 cells=dict(values=[dct.get(i)['Year&Week'],dct.get(i)['Activity'],dct.get(i)['MM/WF CODE'],dct.get(i)['Duration'],dct.get(i)['Average'] ],
+                                           line_color='darkslategray',
+                                           fill_color='lightcyan',
+                                           align='left'))])
+                fig.update_layout(
+                    title=f"{i} Table",
+
+
+                )
+
+
+
+
+                st.write(fig)
+                st.dataframe(dct.get(i).drop(['Year','Week'], axis=1))
+        #------------create_figures_of_every_product_in_each_activities
+
+            for i in w :
+
+                fig = px.bar(x= dct.get(i)['Year&Week'],
+                    y= dct.get(i)['Duration'], color=dct.get(i)["MM/WF CODE"], labels={"color": "product name", "x": "time spent","y":"Week"})
+                fig.update_layout(
+                title=f"{i} figure",
+                yaxis_title="Hours",
+                xaxis_title="Date",)
+                fig.update_xaxes(title_font_family="Arial")
+                fig.update_yaxes(title_font_family="Arial")
+                st.write(fig)
+
 
         # --- STREAMLIT SELECTION
         class corp_app :
@@ -284,75 +321,24 @@ if authentication_status :
 
 
             def create_figures(self,data):
-
-                # --- PLOT PIE CHART
-                new_title = '<p style="font-family:sans-serif; color:black; font-size: 30px;">fig theorical and total of hours worked on a week :</p>'
-                st.markdown(new_title, unsafe_allow_html=True)
-                fig = go.Figure()
-                fig.add_trace(go.Scatter(x=data['years'], y=data['Total'],
-                                    mode='lines',
-                                    name='Total worked on a week',
-                                    line_color='#ffe476'))
-
-                fig.add_trace(go.Scatter(x=data['years'], y=data['Theorical per week'],
-                                    mode='markers', name='Theorical per week'))
-
-                fig.update_layout(
-                    xaxis_title="weeks",
-                    yaxis_title="Total",
-                    legend_title="Total & theorical per week",
-                    width=1000, height=500,
-                    xaxis=dict(
-                        rangeselector=dict(
-                            buttons=list([
-                                dict(count=1,
-                                     label="1m",
-                                     step="month",
-                                     stepmode="backward"),
-                                dict(count=6,
-                                     label="6m",
-                                     step="month",
-                                     stepmode="backward"),
-                                dict(count=1,
-                                     label="YTD",
-                                     step="year",
-                                     stepmode="todate"),
-                                dict(count=1,
-                                     label="1y",
-                                     step="year",
-                                     stepmode="backward"),
-                                dict(step="all")
-                            ])
-                        ),
-                        rangeslider=dict(
-
-                            visible=True
-                        )
-                    )
-
-                )
-                fig.update_xaxes(title_font_family="Arial")
-                fig.update_yaxes(title_font_family="Arial")
-                st.write(fig)
-
                 #-----------------Absnece and breaks per year&week
-                new_title = '<p style="font-family:sans-serif; color:black; font-size: 30px;">Absnece and breaks per year&week :</p>'
-                st.markdown(new_title, unsafe_allow_html=True)
+                #new_title = '<p style="font-family:sans-serif; color:black; font-size: 30px;">Absnece and breaks per year&week :</p>'
+                #st.markdown(new_title, unsafe_allow_html=True)
 
-                fig = go.Figure(data=[go.Table(header=dict(values=['years','weeks', 'Absence','breaks']),
-                                 cells=dict(values=[data['years'],
-                                                    data['weeks'],
-                                                    data['Absence'],
-                                                    data['breaks']],
-                                                    line_color='darkslategray',
-                                                    fill_color='lightcyan',
-                                                    align='left',
-                                                    font=dict(color='black', size=12),
-                                                    ))])
-                fig.update_layout(width=1000, height=800,)
-                fig.update_xaxes(title_font_family="Arial")
-                fig.update_yaxes(title_font_family="Arial")
-                st.write(fig)
+                #fig = go.Figure(data=[go.Table(header=dict(values=['years','weeks', 'Absence','breaks']),
+                #                 cells=dict(values=[data['years'],
+                #                                    data['weeks'],
+                #                                    data['Absence'],
+                #                                    data['breaks']],
+                #                                    line_color='darkslategray',
+                #                                    fill_color='lightcyan',
+                #                                    align='left',
+                #                                    font=dict(color='black', size=12),
+                #                                    ))])
+                #fig.update_layout(width=1000, height=800,)
+                #fig.update_xaxes(title_font_family="Arial")
+                #fig.update_yaxes(title_font_family="Arial")
+                #st.write(fig)
 
                 #--------------------Non technical Data per week
                 new_title = '<p style="font-family:sans-serif; color:black; font-size: 30px;">Non technical Data per week :</p>'
@@ -387,13 +373,6 @@ if authentication_status :
                 fig.update_yaxes(title_font_family="Arial")
                 st.write(fig)
 
-                #------------box plot on total
-                new_title = '<p style="font-family:sans-serif; color:black; font-size: 30px;">box plot on total:</p>'
-                st.markdown(new_title, unsafe_allow_html=True)
-                fig = px.box(data, y="Total")
-                fig.update_xaxes(title_font_family="Arial")
-                fig.update_yaxes(title_font_family="Arial")
-                st.write(fig)
 
         def main():
 
